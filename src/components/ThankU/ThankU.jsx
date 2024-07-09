39728d51443dbab1991a91236c461d3041319f85
Dede Mahayana <dedetirtamahayana@gmail.com>
2024-07-09 22:19:14 +0800

    fix image name

--- src/components/ThankU/ThankU.jsx
+++ src/components/ThankU/ThankU.jsx
@@ -11,7 +11,7 @@ const ThankU = () => {
             <p>kami sekeluarga mengucapkan</p>
           </div>
           <div className=' pt-12 pb-24 lg:pt-20 lg:pb-40 ml-7 lg:ml-44'>
-            <img src='/kasih.png' alt='' className='w-[250px] ml-[51px]' />
+            <img src='/Kasih.png' alt='' className='w-[250px] ml-[51px]' />
           </div>
           <div className=' absolute z-10 mt-[1.2rem] ml-12 lg:ml-[6rem]'>
             <img src='/panah-bg.png' alt='' className='w-[25px]' />
@@ -21,7 +21,7 @@ const ThankU = () => {
       <div className='section-2 bg-abuterang border-y-1 border-birutext'>
         <div className='container'>
           <div className='absolute -mt-[0.7rem] ml-24'>
-            <img src='/countdown.png' alt='' className='w-[150px]' />
+            <img src='/Countdown.png' alt='' className='w-[150px]' />
           </div>
           <div>
             <div className='py-20'>
